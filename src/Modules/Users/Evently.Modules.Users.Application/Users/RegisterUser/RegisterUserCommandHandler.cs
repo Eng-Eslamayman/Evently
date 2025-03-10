9017fb46509ee62c4f814ab568b3fb8095951aaf
Eslam ayman <106927185+Eng-Eslamayman@users.noreply.github.com>
2025-03-10 02:52:28 +0200

    added user module and finshed domain and application layer in this module

---
+++ src/Modules/Users/Evently.Modules.Users.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Evently.Common.Application.Data;
+using Evently.Common.Application.Messaging;
+using Evently.Common.Domain;
+using Evently.Modules.Users.Domain.Users;
+using MediatR;
+
+namespace Evently.Modules.Users.Application.Users.RegisterUser;
+
+public class RegisterUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork) : ICommandHandler<RegisterUserCommand, Guid>
+{
+    public async Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        User user = User.Create(request.Email,
+            request.FirstName,
+            request.LastName
+        );
+
+        userRepository.Insert(user);
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return user.Id;
+    }
+}
