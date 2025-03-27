7d2ee3f7bcf1e379ab01f033ff6db24951c0e21a
Eslam ayman <106927185+Eng-Eslamayman@users.noreply.github.com>
2025-03-27 23:44:16 +0200

    added user registration with keyCloak and added identity id column in user table

---
+++ src/Modules/Users/Evently.Modules.Users.Application/Abstractions/Identity/IIdentityProviderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evently.Common.Domain;
+
+namespace Evently.Modules.Users.Application.Abstractions.Identity;
+public interface IIdentityProviderService
+{
+    Task<Result<string>> RegisterUserAsync(UserModel user, CancellationToken cancellationToken = default);
+}
--- src/Modules/Users/Evently.Modules.Users.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
+++ src/Modules/Users/Evently.Modules.Users.Application/Users/RegisterUser/RegisterUserCommandHandler.cs
@@ -1,18 +1,37 @@
 ﻿using Evently.Common.Application.Messaging;
 using Evently.Common.Domain;
 using Evently.Modules.Users.Application.Abstractions.Data;
+using Evently.Modules.Users.Application.Abstractions.Identity;
 using Evently.Modules.Users.Domain.Users;
 using MediatR;
 
 namespace Evently.Modules.Users.Application.Users.RegisterUser;
 
-public class RegisterUserCommandHandler(IUserRepository userRepository, IUnitOfWork unitOfWork) : ICommandHandler<RegisterUserCommand, Guid>
+public class RegisterUserCommandHandler(
+    IUserRepository userRepository, 
+    IIdentityProviderService identityProviderService,
+    IUnitOfWork unitOfWork)
+    : ICommandHandler<RegisterUserCommand, Guid>
 {
     public async Task<Result<Guid>> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
     {
-        User user = User.Create(request.Email,
+        Result<string> result = await identityProviderService.RegisterUserAsync(new UserModel
+        (
+        request.Email,
+        request.Password,
+        request.FirstName,
+        request.LastName
+        ), cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return Result.Failure<Guid>(result.Error);
+        }
+
+        var user = User.Create(request.Email,
             request.FirstName,
-            request.LastName
+            request.LastName,
+            result.Value
         );
 
         userRepository.Insert(user);
